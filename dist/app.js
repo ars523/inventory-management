d5db3a1ab9b8afd2780d3bc666da38e61f4bff56
Arifur Rahaman <arifur.rahaman.siflu@gmail.com>
2024-05-22 17:41:42 +0600

    added route to handle invalid routes

--- dist/app.js
+++ dist/app.js
@@ -9,6 +9,14 @@ const order_route_1 = require("./modules/order/order.route");
 const app = (0, express_1.default)();
 //Parser
 app.use(express_1.default.json());
+//Routes
 app.use("/api/products", product_route_1.productRouter);
 app.use("/api/orders", order_route_1.orderRouter);
+// Error handling for invalid routes
+app.use("/*", (req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found",
+    });
+});
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { productRouter } from "./modules/product/product.route";
 import { orderRouter } from "./modules/order/order.route";
 const app = express();
@@ -6,7 +6,16 @@ const app = express();
 //Parser
 app.use(express.json());
 
+//Routes
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+
+// Error handling for invalid routes
+app.use("/*", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
 
 export default app;
